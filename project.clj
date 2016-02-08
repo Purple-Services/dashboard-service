f72a1a36320417188c38ae5cbca233e9cbb3da72
James Borden <jmborden@gmail.com>
2016-02-08 13:42:34 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dashboard-clj "0.1.0-SNAPSHOT"
+  :description "Dashboard server"
+  :url "http://purpledelivery.com"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-json "0.1.2"]])
---
+++ test/dashboard_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns dashboard-clj.core-test
+  (:require [clojure.test :refer :all]
+            [dashboard-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
