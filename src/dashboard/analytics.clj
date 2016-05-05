68dbd10ad92f5bd1c9fa37ab2f291260e4ab8566
James <jmborden@gmail.com>
2016-05-05 11:42:53 -0500

    sql updated for orders per day to exclude incorrect dates in event_log that date back to 1974

--- src/dashboard/analytics.clj
+++ src/dashboard/analytics.clj
@@ -204,7 +204,7 @@
   [db-conn]
   (let [orders-per-day-result (sql/with-connection db-conn
                                 (sql/with-query-results results
-                                  ["select from_unixtime(substr(event_log,locate('complete',event_log)+ 9,10),'%Y-%m-%d') as 'date',COUNT(DISTINCT id) as 'orders' from orders where status = 'complete' GROUP BY from_unixtime(substr(event_log,locate('complete',event_log)+ 9,10),'%Y-%m-%d')"]
+                                  ["select from_unixtime(substr(event_log,locate('complete',event_log)+ 9,10),'%Y-%m-%d') as 'date',COUNT(DISTINCT id) as 'orders' from orders where status = 'complete' AND  substr(event_log,locate('complete',event_log) + 9, 10) > 1420070400 GROUP BY from_unixtime(substr(event_log,locate('complete',event_log)+ 9,10),'%Y-%m-%d')"]
                                   (doall results)))
         processed-orders
         (->> orders-per-day-result
