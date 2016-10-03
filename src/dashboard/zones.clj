04e2388c9ffa683acc563683d4aeae3d2ade9f02
James <jmborden@gmail.com>
2016-10-03 17:59:23 -0500

    get/put for zones updated

--- src/dashboard/zones.clj
+++ src/dashboard/zones.clj
@@ -18,6 +18,7 @@
                            (read-string (:service_fees zone)))
             :service_time_bracket (read-string
                                    (:service_time_bracket zone))))
+
 (defn get-all-zones-from-db
   "Retrieve all zones from the DB"
   [db-conn]
@@ -33,70 +34,95 @@
          results)))
 
 (defn get-zone-by-id
+  "Return a zone as expected by the dashboard client"
   [db-conn id]
-  (first (!select db-conn
-                  "zones"
-                  ["*"]
-                  {:id id})))
+  (let [zone (raw-sql-query
+              db-conn
+              [(str "SELECT zones.id as `id`, zones.name as `name`,"
+                    "zones.rank as `rank`,zones.active as `active`,"
+                    "zones.config as `config`, "
+                    "GROUP_CONCAT(distinct zips.zip) as `zips`, "
+                    "COUNT(DISTINCT zips.zip) as `zip_count` FROM `zones` "
+                    "LEFT JOIN zips ON FIND_IN_SET (zones.id,zips.zones) "
+                    "WHERE zones.id = " id " "
+                    "GROUP BY zones.id;"
+                    )])]
+    (first zone)))
+
+#_ (def zone-validations
+     {:price-87 [[v/required :message "87 Octane price can not be blank!"]
+                 [v/number
+                  :message "87 Octance price must be in a dollar amount"]
+                 [v/in-range [1 5000] :message
+                  "Price must be within $0.01 and $50.00"]]
+      :price-91 [[v/required :message "91 Octane price can not be blank!"]
+                 [v/number
+                  :message "91 Octance price must be in a dollar amount"]
+                 [v/in-range [1 5000] :message
+                  "Price must be within $0.01 and $50.00"]]
+      :service-fee-60 [[v/required :message
+                        "One hour service fee can not be blank!"]
+                       [v/number
+                        :message
+                        "One hour service fee must be in a dollar amount"]
+                       [v/in-range [0 5000] :message
+                        "Price must be within $0.00 and $50.00"]]
+      :service-fee-180 [[v/required :message
+                         "Three hour service fee can not be blank!"]
+                        [v/number
+                         :message
+                         "Three hour service fee must be in a dollar amount"]
+                        [v/in-range [0 5000] :message
+                         "Price must be within $0.00 and $50.00"]]
+      :service-fee-300 [[v/required :message
+                         "Five hour service fee can not be blank!"]
+                        [v/number
+                         :message
+                         "Five hour service fee must be in a dollar amount"]
+                        [v/in-range [0 5000] :message
+                         "Price must be within $0.00 and $50.00"]]
+      :service-time-bracket-begin [[v/required :message
+                                    "Begin time can not be blank!"]
+                                   [v/number
+                                    :message "Service time must be a number"]
+                                   [v/integer
+                                    :message
+                                    "Service time must be a whole number!"]
+                                   [v/in-range [0 1440]
+                                    :message
+                                    "Service time must be between 0 and 1440"]]
+      :service-time-bracket-end [[v/required :message
+                                  "End time can not be blank!"]
+                                 [v/number
+                                  :message "Service time must be a number"]
+                                 [v/integer
+                                  :message
+                                  "Service time must be a whole number!"]
+                                 [v/in-range [0 1440]
+                                  :message
+                                  "Service time must be between 0 and 1440"]]})
 
 (def zone-validations
-  {:price-87 [[v/required :message "87 Octane price can not be blank!"]
-              [v/number
-               :message "87 Octance price must be in a dollar amount"]
-              [v/in-range [1 5000] :message
-               "Price must be within $0.01 and $50.00"]]
-   :price-91 [[v/required :message "91 Octane price can not be blank!"]
-              [v/number
-               :message "91 Octance price must be in a dollar amount"]
-              [v/in-range [1 5000] :message
-               "Price must be within $0.01 and $50.00"]]
-   :service-fee-60 [[v/required :message
-                     "One hour service fee can not be blank!"]
-                    [v/number
-                     :message
-                     "One hour service fee must be in a dollar amount"]
-                    [v/in-range [0 5000] :message
-                     "Price must be within $0.00 and $50.00"]]
-   :service-fee-180 [[v/required :message
-                      "Three hour service fee can not be blank!"]
-                     [v/number
-                      :message
-                      "Three hour service fee must be in a dollar amount"]
-                     [v/in-range [0 5000] :message
-                      "Price must be within $0.00 and $50.00"]]
-   :service-fee-300 [[v/required :message
-                      "Five hour service fee can not be blank!"]
-                     [v/number
-                      :message
-                      "Five hour service fee must be in a dollar amount"]
-                     [v/in-range [0 5000] :message
-                      "Price must be within $0.00 and $50.00"]]
-   :service-time-bracket-begin [[v/required :message
-                                 "Begin time can not be blank!"]
-                                [v/number
-                                 :message "Service time must be a number"]
-                                [v/integer
-                                 :message
-                                 "Service time must be a whole number!"]
-                                [v/in-range [0 1440]
-                                 :message
-                                 "Service time must be between 0 and 1440"]]
-   :service-time-bracket-end [[v/required :message
-                               "End time can not be blank!"]
-                              [v/number
-                               :message "Service time must be a number"]
-                              [v/integer
-                               :message
-                               "Service time must be a whole number!"]
-                              [v/in-range [0 1440]
-                               :message
-                               "Service time must be between 0 and 1440"]]})
+  {:name [[v/required :message "Name can not be blank!"]
+          [v/string :message "Name must be a string"]]
+   :rank [[v/required :message "Rank can not be blank!"]
+          [v/integer :message "Rank must be a whole number"]
+          [v/in-range [1 10000] :message
+           "Rank must be between 1 and 10000"]]
+   :active [[v/required :message "Active must be present"]
+            [v/boolean :message "Active must be either true or false"]]})
 
 (defn validate-and-update-zone!
   "Given a zone map, validate it. If valid, update zone else return the
   bouncer error map.
 
   Update fields are fuel_prices, service_fees and service_time_bracket
+
+  name is a string
+
+  rank is an integer with a value between 1 and 10000
+
+  active? is a boolean
 
   fuel-prices is an edn string map of the format
   '{:87 <integer cents> :91 <integer cents>}'.
@@ -115,20 +141,12 @@
   10:30pm is represened as 1350 which is (+ (* 60 22) 30)"
   [db-conn zone]
   (if (b/valid? zone zone-validations)
-    (let [{:keys [price-87 price-91 service-fee-60 service-fee-180 service-fee-300
-                  service-time-bracket-begin service-time-bracket-end id]} zone
-                  update-result (!update db-conn "zones"
-                                         {:fuel_prices
-                                          (str {:87 price-87
-                                                :91 price-91})
-                                          :service_fees
-                                          (str {:60 service-fee-60
-                                                :180 service-fee-180
-                                                :300 service-fee-300})
-                                          :service_time_bracket
-                                          (str [service-time-bracket-begin
-                                                service-time-bracket-end])}
-                                         {:id (:id zone)})]
+    (let [{:keys [id name rank active]} zone
+          update-result (!update db-conn "zones"
+                                 {:name name
+                                  :rank rank
+                                  :active active}
+                                 {:id id})]
       (if (:success update-result)
         (assoc update-result :id id)
         update-result))
