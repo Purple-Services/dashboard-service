a4556444cfffeb1ecdd4f8913cbeafbfa4bd83e2
Christopher Elwell <elwell.christopher@gmail.com>
2016-10-12 13:25:12 -0700

    make zones show on dashmap. add color column to zones table

--- src/dashboard/zones.clj
+++ src/dashboard/zones.clj
@@ -30,8 +30,8 @@
         (raw-sql-query
          db-conn
          [(str "SELECT zones.id as `id`, zones.name as `name`,"
-               "zones.rank as `rank`,zones.active as `active`,"
-               "zones.config as `config`, "
+               "zones.rank as `rank`, zones.active as `active`,"
+               "zones.color as `color`, zones.config as `config`, "
                "GROUP_CONCAT(distinct zips.zip) as `zips`, "
                "COUNT(DISTINCT zips.zip) as `zip_count` FROM `zones` "
                "LEFT JOIN zips ON FIND_IN_SET (zones.id,zips.zones) "
