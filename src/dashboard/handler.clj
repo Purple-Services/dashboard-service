5e97e1e7eada8f8d1a5220ce34db86d63ad30da1
Christopher Elwell <elwell.christopher@gmail.com>
2016-10-15 00:36:15 -0700

    allow invalid zip-def on zip search to help with diagnosing issues

--- src/dashboard/handler.clj
+++ src/dashboard/handler.clj
@@ -470,11 +470,11 @@
         (into [] (get-all-zones-from-db (conn)))))
   ;; return all zips we will use
   (GET "/zips/:zip" [zip]
-       (let [zip-result (zones/get-zip-def (conn) (str zip))]
+       (let [zip-result (zones/get-zip-def-not-validated (conn) (str zip))]
          (response
           (if (nil? zip-result)
             {}
-            (zones/get-zip-def (conn) (str zip))))))
+            zip-result))))
   ;; return zcta defintions for zips
   (POST "/zctas" {body :body}
         (response
