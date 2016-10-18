cafc7d097c28eaae6cda7a61e706a8ba6f06eed4
James <jmborden@gmail.com>
2016-10-18 16:17:12 -0500

    look for actual zips instead of just nil

--- test/dashboard/test/zones.clj
+++ test/dashboard/test/zones.clj
@@ -70,7 +70,7 @@
         _ (zones/create-zone! db-conn {:name "San Diego"
                                        :rank 100
                                        :active true
-                                       :zips "91901, 91910, 91911, 91913, 91932, 91935, 91941, 91942, 91945, 91950, 91977, 91978, 92007, 92008, 92009, 92010, 92011, 92014, 92019, 92020, 92021, 92024, 92037, 92040, 92054, 92067, 92071, 92075, 92091, 92101, 92102, 92103, 92104, 92105, 92106, 92107, 92108, 92109, 92110, 92111, 92113, 92114, 92115, 92116, 92117, 92119, 92120, 92121, 92122, 92123, 92124, 92126, 92129, 92130, 92131, 92140, 92154"
+                                       :zips "91901,91910,91911,91913,91932,91935,91941,91942,91945,91950,91977,91978,92007,92008,92009,92010,92011,92014,92019,92020,92021,92024,92037,92040,92054,92067,92071,92075,92091,92101,92102,92103,92104,92105,92106,92107,92108,92109,92110,92111,92113,92114,92115,92116,92117,92119,92120,92121,92122,92123,92124,92126,92129,92130,92131,92140,92154"
                                        :config (str
                                                 {:gas-price {"87" 300, "91" 316},
                                                  :hours [[[420 1230]] [[420 1230]] [[420 1230]] [[420 1230]] [[420 1230]] [[780 1020]] []],
@@ -83,34 +83,39 @@
         _ (zones/create-zone! db-conn {:name "La Jolla"
                                        :rank 1000
                                        :active true
-                                       :zips "92037, 92108, 92111, 92117, 92121, 92122, 92123, 92126, 92131"
+                                       :zips "92037,92108,92111,92117,92121,92122,92123,92126,92131"
                                        :config (str {:manually-closed? false})})
         la-jolla (first (db/!select db-conn "zones" ["*"] {:name "La Jolla"}))
         ;; create El Cajon
         _ (zones/create-zone! db-conn {:name "El Cajon"
                                        :rank 100
                                        :active true
-                                       :zips "91901, 91935, 91941, 91942, 91945, 91977, 91978, 92019, 92020, 92021, 92040, 92071, 92114, 92115, 92119, 92120, 92124"
+                                       :zips "91901,91935,91941,91942,91945,91977,91978,92019,92020,92021,92040,92071,92114,92115,92119,92120,92124"
 
                                        :config (str {:manually-closed? false})})
         el-cajon (first (db/!select db-conn "zones" ["*"] {:name "El Cajon"}))]
-    ;; La Jolla should not have nil zips
-    (is (not (nil? (:zips (zones/get-zone-by-name db-conn "La Jolla")))))
-    ;; but neither should San Diego
-    (is (not (nil? (:zips (zones/get-zone-by-name db-conn "San Diego")))))
-    ;; remove 92131 from La Jolla
-    (zones/update-zone!
-     db-conn
-     (assoc (zones/get-zone-by-name db-conn "La Jolla")
-            :zips "92037, 92108, 92111, 92117, 92121, 92122, 92123, 92126"))
-    ;; La Jolla should not have nil zips
-    (is (not (nil? (:zips (zones/get-zone-by-name db-conn "La Jolla")))))
-    ;; La Jolla should have one less zip
-    (is (= "92037,92108,92111,92117,92121,92122,92123,92126"
-           (:zips (zones/get-zone-by-name db-conn "La Jolla"))))
-    ;; And San Diego should not be affected
-    (is (not (nil? (:zips (zones/get-zone-by-name db-conn "San Diego")))))
-    ))
+    (testing "A zone's config man be modified without affecting other zones"
+      ;; update San Diego's config
+      (zones/update-zone! db-conn
+                          (assoc (zones/get-zone-by-name db-conn "San Diego")
+                                 :config (str {:manually-closed? true})))
+      ;; La Jolla and San Diego should have the same zips
+      (is (=  "92037,92108,92111,92117,92121,92122,92123,92126,92131"
+              (:zips (zones/get-zone-by-name db-conn "La Jolla"))))
+      (is (= "91901,91910,91911,91913,91932,91935,91941,91942,91945,91950,91977,91978,92007,92008,92009,92010,92011,92014,92019,92020,92021,92024,92037,92040,92054,92067,92071,92075,92091,92101,92102,92103,92104,92105,92106,92107,92108,92109,92110,92111,92113,92114,92115,92116,92117,92119,92120,92121,92122,92123,92124,92126,92129,92130,92131,92140,92154"
+             (:zips (zones/get-zone-by-name db-conn "San Diego")))))
+    (testing "A zone's zips can be removed without affecting other zones"
+      ;; remove 92131 from La Jolla
+      (zones/update-zone!
+       db-conn
+       (assoc (zones/get-zone-by-name db-conn "La Jolla")
+              :zips "92037,92108,92111,92117,92121,92122,92123,92126"))
+      ;; La Jolla should have one less zip
+      (is (= "92037,92108,92111,92117,92121,92122,92123,92126"
+             (:zips (zones/get-zone-by-name db-conn "La Jolla"))))
+      ;; And San Diego should not be affected
+      (is (= "91901,91910,91911,91913,91932,91935,91941,91942,91945,91950,91977,91978,92007,92008,92009,92010,92011,92014,92019,92020,92021,92024,92037,92040,92054,92067,92071,92075,92091,92101,92102,92103,92104,92105,92106,92107,92108,92109,92110,92111,92113,92114,92115,92116,92117,92119,92120,92121,92122,92123,92124,92126,92129,92130,92131,92140,92154"
+             (:zips (zones/get-zone-by-name db-conn "San Diego")))))))
 
 
 (deftest manually-created-zips
